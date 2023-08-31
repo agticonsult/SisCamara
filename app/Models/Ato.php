fdf79f229435f018c1930caa3f59ea1d4fc99485
leonir12 <leonirmiller@gmail.com>
2023-08-31 10:47:50 -0400

    add filtros em ato publico

--- app/Models/Ato.php
+++ app/Models/Ato.php
@@ -78,6 +78,59 @@ class Ato extends Model implements Auditable
         return $atos;
         // return $this->hasMany(LinhaAto::class, 'id_ato', 'id')->where('ativo', '=', 1);
     }
+
+    public function buscar($filtro = null)
+    {
+
+        $resultados = $this->where(function($query) use($filtro){
+
+            if(empty(collect($filtro)->except('_token', '_method') === null)){
+                $query->get();
+            }
+
+            if(isset($filtro['id_classificacao'])){
+                $query->where('id_classificacao', '=', $filtro['id_classificacao']);
+            }
+
+            if(isset($filtro['ano'])){
+                $query->where('ano', 'ILIKE', '%' . $filtro['ano'] . '%');
+            }
+
+            if(isset($filtro['numero'])){
+                $query->where('numero', 'ILIKE', '%' . $filtro['numero'] . '%');
+            }
+
+            if(isset($filtro['id_tipo_ato'])){
+                $query->where('id_tipo_ato', '=', $filtro['id_tipo_ato']);
+            }
+
+            if(isset($filtro['id_assunto'])){
+                $query->where('id_assunto', '=', $filtro['id_assunto']);
+            }
+
+            if(isset($filtro['altera_dispositivo'])){
+                $query->where('altera_dispositivo', '=', $filtro['altera_dispositivo']);
+            }
+
+            if(isset($filtro['id_orgao'])){
+                $query->where('id_orgao', '=', $filtro['id_orgao']);
+            }
+
+            if(isset($filtro['id_forma_publicacao'])){
+                $query->where('id_forma_publicacao', '=', $filtro['id_forma_publicacao']);
+            }
+
+        })->where(function($subquery) use($filtro){
+
+            if(isset($filtro['data_publicacao'])){
+                $subquery->where('data_publicacao', 'ILIKE', $filtro['data_publicacao'] . '%');
+            }
+
+        })
+        ->paginate(20);
+
+        return $resultados;
+    }
 }
 
 
