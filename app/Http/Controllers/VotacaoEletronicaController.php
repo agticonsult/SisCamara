5f48289e66d81032fcda57508dde30dc1b84e2fc
rodrigodonche <rodrigodonche@gmail.com>
2023-09-14 15:19:16 -0400

    votacao eletronica index

--- app/Http/Controllers/VotacaoEletronicaController.php
+++ app/Http/Controllers/VotacaoEletronicaController.php
@@ -6,6 +6,7 @@ use App\Models\ErrorLog;
 use App\Models\VotacaoEletronica;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class VotacaoEletronicaController extends Controller
 {
@@ -33,206 +34,206 @@ class VotacaoEletronicaController extends Controller
         }
     }
 
-    public function create()
-    {
-        try {
-            if(Auth::user()->temPermissao('VotacaoEletronica', 'Listagem') != 1){
-                return redirect()->back()->with('erro', 'Acesso negado.');
-            }
+    // public function create()
+    // {
+    //     try {
+    //         if(Auth::user()->temPermissao('VotacaoEletronica', 'Listagem') != 1){
+    //             return redirect()->back()->with('erro', 'Acesso negado.');
+    //         }
 
-            return view('votacao-eletronica.create');
-        }
-        catch (\Exception $ex) {
-            $erro = new ErrorLog();
-            $erro->erro = $ex->getMessage();
-            $erro->controlador = "VotacaoEletronicaController";
-            $erro->funcao = "create";
-            if (Auth::check()){
-                $erro->cadastradoPorUsuario = auth()->user()->id;
-            }
-            $erro->save();
-            return redirect()->back()->with('erro', 'Contate o administrador do sistema.');
-        }
-    }
+    //         return view('votacao-eletronica.create');
+    //     }
+    //     catch (\Exception $ex) {
+    //         $erro = new ErrorLog();
+    //         $erro->erro = $ex->getMessage();
+    //         $erro->controlador = "VotacaoEletronicaController";
+    //         $erro->funcao = "create";
+    //         if (Auth::check()){
+    //             $erro->cadastradoPorUsuario = auth()->user()->id;
+    //         }
+    //         $erro->save();
+    //         return redirect()->back()->with('erro', 'Contate o administrador do sistema.');
+    //     }
+    // }
 
-    public function store(Request $request)
-    {
-        try {
-            if(Auth::user()->temPermissao('VotacaoEletronica', 'Listagem') != 1){
-                return redirect()->back()->with('erro', 'Acesso negado.');
-            }
+    // public function store(Request $request)
+    // {
+    //     try {
+    //         if(Auth::user()->temPermissao('VotacaoEletronica', 'Listagem') != 1){
+    //             return redirect()->back()->with('erro', 'Acesso negado.');
+    //         }
 
-            $input = [
-                'assunto' => $request->assunto,
-                'conteudo' => $request->conteudo,
-            ];
-            $rules = [
-                'assunto' => 'required',
-                'conteudo' => 'required',
-            ];
+    //         $input = [
+    //             'assunto' => $request->assunto,
+    //             'conteudo' => $request->conteudo,
+    //         ];
+    //         $rules = [
+    //             'assunto' => 'required',
+    //             'conteudo' => 'required',
+    //         ];
 
-            $validar = Validator::make($input, $rules);
-            $validar->validate();
+    //         $validar = Validator::make($input, $rules);
+    //         $validar->validate();
 
-            $modelo_documento = new ModeloDocumento();
-            $modelo_documento->assunto = $request->assunto;
-            $modelo_documento->conteudo = $request->conteudo;
-            $modelo_documento->cadastradoPorUsuario = Auth::user()->id;
-            $modelo_documento->ativo = 1;
-            $modelo_documento->save();
+    //         $modelo_documento = new ModeloDocumento();
+    //         $modelo_documento->assunto = $request->assunto;
+    //         $modelo_documento->conteudo = $request->conteudo;
+    //         $modelo_documento->cadastradoPorUsuario = Auth::user()->id;
+    //         $modelo_documento->ativo = 1;
+    //         $modelo_documento->save();
 
-            return redirect()->route('votacao-eletronica.index')->with('success', 'Cadastro realizado com sucesso');
-        }
-        catch (ValidationException $e ) {
-            $message = $e->errors();
-            return redirect()->back()
-                ->withErrors($message)
-                ->withInput();
-        }
-        catch (\Exception $ex) {
-            $erro = new ErrorLog();
-            $erro->erro = $ex->getMessage();
-            $erro->controlador = "VotacaoEletronicaController";
-            $erro->funcao = "store";
-            if (Auth::check()){
-                $erro->cadastradoPorUsuario = auth()->user()->id;
-            }
-            $erro->save();
-            return redirect()->back()->with('erro', 'Contate o administrador do sistema.');
-        }
-    }
+    //         return redirect()->route('votacao-eletronica.index')->with('success', 'Cadastro realizado com sucesso');
+    //     }
+    //     catch (ValidationException $e ) {
+    //         $message = $e->errors();
+    //         return redirect()->back()
+    //             ->withErrors($message)
+    //             ->withInput();
+    //     }
+    //     catch (\Exception $ex) {
+    //         $erro = new ErrorLog();
+    //         $erro->erro = $ex->getMessage();
+    //         $erro->controlador = "VotacaoEletronicaController";
+    //         $erro->funcao = "store";
+    //         if (Auth::check()){
+    //             $erro->cadastradoPorUsuario = auth()->user()->id;
+    //         }
+    //         $erro->save();
+    //         return redirect()->back()->with('erro', 'Contate o administrador do sistema.');
+    //     }
+    // }
 
-    public function edit($id)
-    {
-        try {
-            if(Auth::user()->temPermissao('VotacaoEletronica', 'Alteração') != 1){
-                return redirect()->back()->with('erro', 'Acesso negado.');
-            }
+    // public function edit($id)
+    // {
+    //     try {
+    //         if(Auth::user()->temPermissao('VotacaoEletronica', 'Alteração') != 1){
+    //             return redirect()->back()->with('erro', 'Acesso negado.');
+    //         }
 
-            $modelo_documento = ModeloDocumento::where('id', '=', $id)->where('ativo', '=', 1)->first();
-            if (!$modelo_documento){
-                return redirect()->back()->with('erro', 'Modelo inválido.');
-            }
+    //         $modelo_documento = ModeloDocumento::where('id', '=', $id)->where('ativo', '=', 1)->first();
+    //         if (!$modelo_documento){
+    //             return redirect()->back()->with('erro', 'Modelo inválido.');
+    //         }
 
-            return view('votacao-eletronica.edit', compact('modelo_documento'));
-        }
-        catch (\Exception $ex) {
-            $erro = new ErrorLog();
-            $erro->erro = $ex->getMessage();
-            $erro->controlador = "VotacaoEletronicaController";
-            $erro->funcao = "edit";
-            if (Auth::check()){
-                $erro->cadastradoPorUsuario = auth()->user()->id;
-            }
-            $erro->save();
-            return redirect()->back()->with('erro', 'Contate o administrador do sistema.');
-        }
-    }
+    //         return view('votacao-eletronica.edit', compact('modelo_documento'));
+    //     }
+    //     catch (\Exception $ex) {
+    //         $erro = new ErrorLog();
+    //         $erro->erro = $ex->getMessage();
+    //         $erro->controlador = "VotacaoEletronicaController";
+    //         $erro->funcao = "edit";
+    //         if (Auth::check()){
+    //             $erro->cadastradoPorUsuario = auth()->user()->id;
+    //         }
+    //         $erro->save();
+    //         return redirect()->back()->with('erro', 'Contate o administrador do sistema.');
+    //     }
+    // }
 
-    public function update(Request $request, $id)
-    {
-        try {
-            if(Auth::user()->temPermissao('VotacaoEletronica', 'Alteração') != 1){
-                return redirect()->back()->with('erro', 'Acesso negado.');
-            }
+    // public function update(Request $request, $id)
+    // {
+    //     try {
+    //         if(Auth::user()->temPermissao('VotacaoEletronica', 'Alteração') != 1){
+    //             return redirect()->back()->with('erro', 'Acesso negado.');
+    //         }
 
-            $input = [
-                'id' => $id,
-                'assunto' => $request->assunto,
-                'conteudo' => $request->conteudo,
-            ];
-            $rules = [
-                'id' => 'required|integer',
-                'assunto' => 'required',
-                'conteudo' => 'required',
-            ];
+    //         $input = [
+    //             'id' => $id,
+    //             'assunto' => $request->assunto,
+    //             'conteudo' => $request->conteudo,
+    //         ];
+    //         $rules = [
+    //             'id' => 'required|integer',
+    //             'assunto' => 'required',
+    //             'conteudo' => 'required',
+    //         ];
 
-            $validar = Validator::make($input, $rules);
-            $validar->validate();
+    //         $validar = Validator::make($input, $rules);
+    //         $validar->validate();
 
-            $modelo_documento = ModeloDocumento::where('id', '=', $id)->where('ativo', '=', 1)->first();
-            if (!$modelo_documento){
-                return redirect()->back()->with('erro', 'Modelo inválido.');
-            }
+    //         $modelo_documento = ModeloDocumento::where('id', '=', $id)->where('ativo', '=', 1)->first();
+    //         if (!$modelo_documento){
+    //             return redirect()->back()->with('erro', 'Modelo inválido.');
+    //         }
 
-            $modelo_documento->assunto = $request->assunto;
-            $modelo_documento->conteudo = $request->conteudo;
-            $modelo_documento->save();
+    //         $modelo_documento->assunto = $request->assunto;
+    //         $modelo_documento->conteudo = $request->conteudo;
+    //         $modelo_documento->save();
 
-            return redirect()->route('votacao-eletronica.index')->with('success', 'Alteração realizada com sucesso');
-        }
-        catch (ValidationException $e ) {
-            $message = $e->errors();
-            return redirect()->back()
-                ->withErrors($message)
-                ->withInput();
-        }
-        catch (\Exception $ex) {
-            $erro = new ErrorLog();
-            $erro->erro = $ex->getMessage();
-            $erro->controlador = "VotacaoEletronicaController";
-            $erro->funcao = "update";
-            if (Auth::check()){
-                $erro->cadastradoPorUsuario = auth()->user()->id;
-            }
-            $erro->save();
-            return redirect()->back()->with('erro', 'Contate o administrador do sistema.');
-        }
-    }
+    //         return redirect()->route('votacao-eletronica.index')->with('success', 'Alteração realizada com sucesso');
+    //     }
+    //     catch (ValidationException $e ) {
+    //         $message = $e->errors();
+    //         return redirect()->back()
+    //             ->withErrors($message)
+    //             ->withInput();
+    //     }
+    //     catch (\Exception $ex) {
+    //         $erro = new ErrorLog();
+    //         $erro->erro = $ex->getMessage();
+    //         $erro->controlador = "VotacaoEletronicaController";
+    //         $erro->funcao = "update";
+    //         if (Auth::check()){
+    //             $erro->cadastradoPorUsuario = auth()->user()->id;
+    //         }
+    //         $erro->save();
+    //         return redirect()->back()->with('erro', 'Contate o administrador do sistema.');
+    //     }
+    // }
 
-    public function destroy(Request $request, $id)
-    {
-        try {
-            if (Auth::user()->temPermissao('VotacaoEletronica', 'Exclusão') != 1) {
-                return redirect()->back()->with('erro', 'Acesso negado.');
-            }
+    // public function destroy(Request $request, $id)
+    // {
+    //     try {
+    //         if (Auth::user()->temPermissao('VotacaoEletronica', 'Exclusão') != 1) {
+    //             return redirect()->back()->with('erro', 'Acesso negado.');
+    //         }
 
-            $input = [
-                'motivo' => $request->motivo
-            ];
-            $rules = [
-                'motivo' => 'max:255'
-            ];
+    //         $input = [
+    //             'motivo' => $request->motivo
+    //         ];
+    //         $rules = [
+    //             'motivo' => 'max:255'
+    //         ];
 
-            $validar = Validator::make($input, $rules);
-            $validar->validate();
+    //         $validar = Validator::make($input, $rules);
+    //         $validar->validate();
 
-            $motivo = $request->motivo;
+    //         $motivo = $request->motivo;
 
-            if ($request->motivo == null || $request->motivo == "") {
-                $motivo = "Exclusão pelo usuário.";
-            }
+    //         if ($request->motivo == null || $request->motivo == "") {
+    //             $motivo = "Exclusão pelo usuário.";
+    //         }
 
-            $modelo_documento = ModeloDocumento::where('id', '=', $id)->where('ativo', '=', 1)->first();
-            if (!$modelo_documento){
-                return redirect()->back()->with('erro', 'Modelo inválido.');
-            }
+    //         $modelo_documento = ModeloDocumento::where('id', '=', $id)->where('ativo', '=', 1)->first();
+    //         if (!$modelo_documento){
+    //             return redirect()->back()->with('erro', 'Modelo inválido.');
+    //         }
 
-            $modelo_documento->inativadoPorUsuario = Auth::user()->id;
-            $modelo_documento->dataInativado = Carbon::now();
-            $modelo_documento->motivoInativado = $motivo;
-            $modelo_documento->ativo = 0;
-            $modelo_documento->save();
+    //         $modelo_documento->inativadoPorUsuario = Auth::user()->id;
+    //         $modelo_documento->dataInativado = Carbon::now();
+    //         $modelo_documento->motivoInativado = $motivo;
+    //         $modelo_documento->ativo = 0;
+    //         $modelo_documento->save();
 
-            return redirect()->route('votacao-eletronica.index')->with('success', 'Exclusão realizada com sucesso.');
-        }
-        catch (ValidationException $e) {
-            $message = $e->errors();
-            return redirect()->back()
-                ->withErrors($message)
-                ->withInput();
-        }
-        catch (\Exception $ex) {
-            $erro = new ErrorLog();
-            $erro->erro = $ex->getMessage();
-            $erro->controlador = "AtividadeLazerController";
-            $erro->funcao = "destroy";
-            if (Auth::check()) {
-                $erro->cadastradoPorUsuario = auth()->user()->id;
-            }
-            $erro->save();
-            return redirect()->back()->with('erro', 'Contate o administrador do sistema.')->withInput();
-        }
-    }
+    //         return redirect()->route('votacao-eletronica.index')->with('success', 'Exclusão realizada com sucesso.');
+    //     }
+    //     catch (ValidationException $e) {
+    //         $message = $e->errors();
+    //         return redirect()->back()
+    //             ->withErrors($message)
+    //             ->withInput();
+    //     }
+    //     catch (\Exception $ex) {
+    //         $erro = new ErrorLog();
+    //         $erro->erro = $ex->getMessage();
+    //         $erro->controlador = "AtividadeLazerController";
+    //         $erro->funcao = "destroy";
+    //         if (Auth::check()) {
+    //             $erro->cadastradoPorUsuario = auth()->user()->id;
+    //         }
+    //         $erro->save();
+    //         return redirect()->back()->with('erro', 'Contate o administrador do sistema.')->withInput();
+    //     }
+    // }
 
 }
